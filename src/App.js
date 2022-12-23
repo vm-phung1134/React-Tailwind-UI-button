ed13fe83d2aeee91628ce4fc1910dfc6d1979b13
Võ Minh Phụng _ B1910282 <phucnguyen1134@gmail.com>
2022-12-23 15:48:05 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ButtonType1 from "./Components/ButtonType1";
+import ButtonType2 from "./Components/ButtonType2";
 export default function App() {
   return (
-    <h1 className="">
-      Hello world!
-    </h1>
-  )
+    <>
+      <p className="text-center mt-[10px] text-2xl">Buttons For Tailwind CSS UI</p>
+      <div className="text-center mt-[20px]">
+        <ButtonType1 />
+      </div>
+    </>
+  );
 }
